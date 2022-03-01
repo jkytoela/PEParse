6738555714a070ea68e737f3229e153b11dcbb4d
Jaakko Kytölä <kytola.jaakko@gmail.com>
2022-03-01 20:28:11 +0200

    feat(backend): parse uploaded file and return results

--- backend/go.mod
+++ backend/go.mod
@@ -3,6 +3,7 @@ module first/go
 go 1.17
 
 require (
+	github.com/edsrzf/mmap-go v1.1.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.7.7 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
@@ -14,7 +15,9 @@ require (
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/saferwall/pe v1.1.1 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
+	go.mozilla.org/pkcs7 v0.0.0-20210826202110-33d05740a352 // indirect
 	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
 	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
 	golang.org/x/text v0.3.7 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,26 +1,46 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	peparser "github.com/saferwall/pe"
 )
 
 func upload(c *gin.Context) {
-	_, header, err := c.Request.FormFile("file")
+	file, _, err := c.Request.FormFile("file")
 	if err != nil {
-		c.String(http.StatusBadRequest, fmt.Sprintf("file err : %s", err.Error()))
+		c.String(http.StatusBadRequest, fmt.Sprintf("File error: %s", err.Error()))
 		return
 	}
 
-	filename := header.Filename
+	buf := bytes.NewBuffer(nil)
 
-	fmt.Println(filename)
+	if _, err := io.Copy(buf, file); err != nil {
+		c.String(http.StatusInternalServerError, fmt.Sprintf("Copy error: %s", err.Error()))
+		return
+	}
+
+	pe, err := peparser.NewBytes(buf.Bytes(), nil)
+	if err != nil {
+		c.String(http.StatusInternalServerError, fmt.Sprintf("Reading error: %s", err.Error()))
+		return
+	}
+
+	err = pe.Parse()
+	if err != nil {
+		c.String(http.StatusInternalServerError, fmt.Sprintf("Parsing error: %s", err.Error()))
+		return
+	}
+
+	c.JSON(http.StatusOK, pe)
 }
 
 func main() {
 	r := gin.Default()
 	r.POST("/upload", upload)
-	r.Run()
+	r.Run("0.0.0.0:8080")
 }
